7ae2e6ee9bc0591f5d948a0f9bfc7bb6fdba39b6
Devin <devinatkin@gmail.com>
2023-08-15 22:21:46 -0600

    made clocked

--- src/tt_um_currymottled.v
+++ src/tt_um_currymottled.v
@@ -1,7 +1,7 @@
 module tt_um_currymottled
 (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    output reg [7:0] uo_out,    // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
@@ -10,14 +10,20 @@ module tt_um_currymottled
     input  wire       rst_n     // reset_n - low to reset
 );
 
-    // 8-bit Adder Logic
+    // 8-bit Adder Logic (combinational part)
     wire [7:0] sum;       // Sum of ui_in and uio_in
     assign sum = ui_in + uio_in;
 
     // Configure uio_oe to set the uio_in as inputs (active low)
     assign uio_oe = 8'b0;
 
-    // Assign the sum to uo_out
-    assign uo_out = (ena) ? sum : 8'b0; // If 'ena' is high, uo_out gets the sum, else 0
+    // Clocked Adder Logic with Synchronous Reset
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            uo_out <= 8'b0; // Synchronous reset
+        end else if (ena) begin
+            uo_out <= sum;  // Capture the sum if enabled
+        end
+    end
 
 endmodule
